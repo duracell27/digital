2fc8545d5c66d64f109b427d839f213265427d86
duracell27 <volodymyrshmidt@gmail.com>
2018-11-30 18:20:44 +0200

    price

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,42 @@
 $(function() {
 
-	  $('button.hamburger').click(function(){
+	$('button.hamburger').click(function(){
 		$(this).toggleClass("is-active");
 	});
 
 
 	$('.on-top').click(function(){
-    	$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
- 	 });
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
 
 	$(document).ready(function(){
 		var width = $(window).width();
-     		var calculated = (width/2)+80;
- 			 $('.anita .right-col').css('width', calculated); 
-    	$(window).resize(function(){
-        	var width = $(window).width();
-     		var calculated = (width/2)+80;
- 			 $('.anita .right-col').css('width', calculated);
-    	});
+		var calculated = (width/2)+80;
+		$('.anita .right-col').css('width', calculated); 
+		$(window).resize(function(){
+			var width = $(window).width();
+			var calculated = (width/2)+80;
+			$('.anita .right-col').css('width', calculated);
+		});
 	});
+
+	$(document).ready(function(){
+		var width1 = $('.price .wrapper .left-bg').width();
+		var width2 = $('.price .content .left-side').width();
+		console.log(width1);
+		console.log(width2);
+		var rezult = width1 - width2;
+		$('.price .content').css('left', rezult);
+		$(window).resize(function(){
+			var width1 = $('.price .wrapper .left-bg').width();
+			var width2 = $('.price .content .left-side').width();
+			console.log(width1);
+			console.log(width2);
+			var rezult = width1 - width2;
+			$('.price .content').css('left', rezult);
+		});
+	});
+
+
 
 });
